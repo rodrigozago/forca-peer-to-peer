cb17ced6310b9b512e399dda9c2471b039bc45c6
Rodrigo Zago <rodrigozago@me.com>
2018-12-17 22:45:29 -0200

    Jogo

--- venv/forca.py
+++ venv/forca.py
@@ -6,7 +6,7 @@ Author: Rodrigo Oliveira e Hyago Henrique
 """
 
 #npy
-import npyscreen
+#import npyscreen
 
 import socket
 from _thread import *
@@ -16,21 +16,20 @@ import time
 
 #portas
 anuncio_do_jogo_broadcast = 4000 #porta para anuncio de novo jogo e início do jogo
-conexoes_jogo_tcp = 4001 #porta para conexao no jogo sobre TCP
+conexoes_jogo_tcp = 4002 #porta para conexao no jogo sobre TCP
 
 
 
 #variaveis de controle
 controle_procurando_jogadores = b'procurando'
 conectar_ao_jogo = b'conectar'
+iniciar_jogo = "init"
 tipo_jogo = 0
 tipo_jogador = 1
 
+dica_global = " "
 #variaveis compartilhadas
-# -> variaveis de trava
-trava_procurar_jogadores = False
-trava_aguardar_jogadores = False
-trava_iniciar_jogo = threading.Condition()
+evento_iniciar_jogo = threading.Event()
 #trava_escutar_tcp = 
 
 #funcao para recuperar ip do host
@@ -41,45 +40,33 @@ def get_ip():
     return local_ip_address
 
 
-#interfaces
-class Inicio(npyscreen.Form):
-    def create(self):
-       self.tipo_de_par = self.add(npyscreen.TitleSelectOne, name= "Você deseja: ", values = ["Jogar","Coordenar"], scroll_exit=True)
-    
-    def activate(self):
-        self.edit()
-        if self.tipo_de_par.value[0] == 0:
-            self.parentApp.setNextForm(PROCURANDO_JOGADORES)
+class Payload:
+    def __init__(self, iniciar_jogo = "N", finalizar_jogo = "N", dica = "N", ganhador = "N", palavra = "N", chute ="N", letra = "N", str = "N"):
+        self.iniciar_jogo = iniciar_jogo
+        self.finalizar_jogo = finalizar_jogo
+        self.dica = dica
+        self.ganhador = ganhador
+        self.palavra = palavra
+
+        self.chute = chute
+        self.letra = letra
+
+        if(str != "N"):
+            self.new_from_str(str)
+    def new_from_str(self, str):
+        array = str.split("#")
+        self.iniciar_jogo = array[0]
+        self.finalizar_jogo = array[1]
+        self.dica = array[2]
+        self.ganhador = array[3]
+        self.palavra = array[4]
+        self.chute = array[5]
+        self.letra = array[6]
+    def __str__(self):
+        return self.iniciar_jogo+"#"+self.finalizar_jogo+"#"+self.dica+"#"+self.ganhador+"#"+self.palavra+"#"+self.chute+"#"+self.letra
 
 
-class ProcurandoJogadores(npyscreen.Form):
-    def create(self):
-        self.texto_procurando_jogadores = self.add(npyscreen.TitleText, name="Procurando jogadores... ",)
-        self.texto_jogadores_conectados = self.add(npyscreen.FixedText, name="Jogadores conectados: \n\tlocalhost:4556 \n\tlocalhost:4984 ",)
-    
-    def activate(self):
-        self.edit()
-        self.parentApp.setNextForm(None)
-
-
-#app
-class Forca(npyscreen.NPSAppManaged):
-   def onStart(self):
-       self.addForm('MAIN', Inicio, name='Forca p2p')
-       self.addForm('PROCURANDO_JOGADORES', ProcurandoJogadores, name="Procurando jogadores")
-       # A real application might define more forms here.......
-       
-
-
-
-    
-
-    
-
-
-
-
-
+payload = Payload()
 class Jogo:
     #construtor
     #passando por parâmetro ao construir objeto
@@ -87,6 +74,7 @@ class Jogo:
     def __init__(self, palavra, dica_inicial):
         self.palavra = palavra
         self.dica_inicial = dica_inicial
+        self.dicas = []
         #cria sockets
 
         #UDP
@@ -105,26 +93,33 @@ class Jogo:
         self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.tcp_socket.bind(("", conexoes_jogo_tcp))
 
+        self.jogadores = []
+        self.evento_procurar_jogadores = threading.Event()
+        self.evento_aguardar_jogadores = threading.Event()
+        self.evento_fim_do_jogo = threading.Event()
+        self.evento_enviar_payload = threading.Event()
+
+
     def procurar_jogadores(self):
-        global trava_procurar_jogadores
+        global evento_procurar_jogadores
         message = get_ip().encode('ascii')
         while True:
-            if trava_procurar_jogadores:
+            if self.evento_procurar_jogadores.is_set():
                 #print("debug: trava ativada, saindo e desativando trava.\n")
                 break
             self.broadcast.sendto(message, ('<broadcast>', anuncio_do_jogo_broadcast))
             #print("debug: mensagem de broadcast enviada.\n")
             time.sleep(5)
-        trava_procurar_jogadores = False
+        self.evento_procurar_jogadores.clear()
         self.broadcast.close()
     
     def aguardar_jogadores(self):
-        global trava_aguardar_jogadores
+        global evento_aguardar_jogadores
         #coloca socket pra escutar
         self.tcp_socket.listen(5)
 
         while True:
-            if trava_aguardar_jogadores:
+            if self.evento_aguardar_jogadores.is_set():
                 # caso a variável de trava seja acionada
                 print("debug: trava ativada, saindo e desativando trava.\n")
                 break
@@ -132,24 +127,35 @@ class Jogo:
             #realiza conexão em nova thread
             client, address = self.tcp_socket.accept() 
             print("debug: conectou tcp aguardar_jogadores.\n")
+            self.jogadores.append(address)
             novaThread = threading.Thread(target=self.conectar_ao_jogador,  args=(client, address))
             novaThread.start()
             
-        trava_aguardar_jogadores = False
+        self.evento_aguardar_jogadores.clear()
 
     def conectar_ao_jogador(self, client, address):
-        print("\nJogando com: ", client.getpeername())
+        global dica_global
+        dicasEnviadas = []
+        #print("Conectado com: %s, aguardando início do jogo."%client.getpeername())
+        evento_iniciar_jogo.wait()
+        #payload enviado com dica inicial e iniciando o jogo
+        fpayload = Payload("1", "N", self.dica_inicial)
+        print("Payload:", fpayload)
+        try:
+            client.send(str(fpayload).encode('ascii'))
+            print("PAYLOAD ENVIADO")
+        except:
+            print("erro no socket ao conectar ao jogador")
         while True:
             data = client.recv(1024)
-            print("%s" %data.decode('ascii'))
-            time.sleep(4)
-            self.tcp_socket.send("Pong".encode('ascii'))
-        #trava_iniciar_jogo.wait()
-        #iniciar jogo...
-        #fechar sockets
-
+            if not data:
+                print("Conexão finalizada.")
+                break
+            else:
+                payload = Payload(str = data.decode('ascii'))
+                print(payload)
     def iniciar_jogo(self):
-        trava_iniciar_jogo.notify_all()
+        evento_iniciar_jogo.set()
 
 
 class Jogador:
@@ -185,50 +191,70 @@ class Jogador:
         try:
             self.tcp_socket.connect((endereco, conexoes_jogo_tcp))
             print("Conectado!")
+            data = self.tcp_socket.recv(1024)
+            print("Iniciando jogo... \nDica inicial: {0}".format(payload.dica))
             while True:
-                self.tcp_socket.send("Ping".encode('ascii'))
-                data = self.tcp_socket.recv(1024)
-                print("%s" %data.decode('ascii'))
+                escolha = input("Escolha uma opção: Letra = 0, Chute = 1")
+                if escolha == "0":
+                    letra = input("Digite a letra: ")
+                    fpayload = Payload(letra=letra)
+                    print("Payload:", fpayload)
+                    self.tcp_socket.send(str(fpayload).encode('ascii'))
+                    self.tcp_socket.recv(1024)
+                elif escolha == "1":
+                    chute = input("Digite o chute: ")
+                    fpayload = Payload(chute=chute)
+                    print("Payload:", fpayload)
+                    self.tcp_socket.send(str(fpayload).encode('ascii'))
+                    self.tcp_socket.recv(1024)
+                
+            
         except:
             print("ERRO! Não foi possível conectar ao jogo. ")
 
 
 
-        
-    
-
-
-
-
-
-
-
 if __name__ == "__main__":
-
-    TestApp = Forca().run()
-
-
-    
-    """
+    #global evento_aguardar_jogadores
+   # global evento_procurar_jogadores
     tipo = input("Digite o tipo: C - Coordenador, J - Jogador.")
     if tipo == "C":
         jogo = Jogo("helloWorld","novaDica")
-    else:
+        print("Novo jogo criado...")
+        tempo = int(input("Digite por quanto tempo aguardar jogadores (segundos): "))
+        print("Aguardando jogadores...")
+        novaThread = threading.Thread(target=jogo.aguardar_jogadores)
+        novaThread.start()
+        novaThread = threading.Thread(target=jogo.procurar_jogadores)
+        novaThread.start()
+        
+        for x in range(0, tempo//5):
+            time.sleep(5)
+            print("Jogadores conectados:")
+            print(jogo.jogadores)
+
+        
+        #finalizar threads
+        print("Finalizando threads:")
+        jogo.evento_procurar_jogadores.set()
+        jogo.evento_aguardar_jogadores.set()
+        jogo.iniciar_jogo()
+        while True:
+            opcao = input("Selecione uma opção: ")
+            if opcao == "1":
+                dica = input("Digite a dica: ")
+                dica_global = dica
+                jogo.evento_enviar_payload.set()
+                
+
+
+    elif tipo =="J":
         jogador = Jogador()
-    while True:
-        opcao = int(input("Digite a opcao desejada:"))
-        if opcao == 0 and tipo == "C":
-            novaThread = threading.Thread(target=jogo.procurar_jogadores)
-            novaThread.start()
-        if opcao == 1 and tipo == "C":
-            trava_procurar_jogadores = True
-        if opcao == 2 and tipo == "C":
-            novaThread = threading.Thread(target=jogo.aguardar_jogadores)
-            novaThread.start()
-        if opcao == 3 and tipo == "C":
-            trava_aguardar_jogadores = True
-        if opcao == 4 and tipo == "J":
-            jogador.procurarJogo()
-    """
+        print("Procurando jogo...")
+        jogador.procurarJogo()
+        #while True:
+    else:
+        print("Opção inválida, saindo")
+        exit()
             
 
